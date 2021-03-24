c9e8d23ea4b424033c14a79684bdea15d140460f
Konstantin Didenko <ironbullyk@gmail.com>
2021-03-23 22:49:22 -0400

    Update config.py

--- rabbit1/config.py
+++ rabbit1/config.py
@@ -1,5 +1,5 @@
-API_TOKEN = '1634010633:AAG6ie9lklBgmu_RbvgkpcsnmLaNY0zOSVU'
-host="localhost"
-user="root"
-password="19216801"
-database="rabbit_schema"
+API_TOKEN = ''
+host=""
+user=""
+password=""
+database=""
